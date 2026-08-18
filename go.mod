7d8a25acce552e16419876a65f99677153a525f8
riazahmedshah <riyazsh360@gmail.com>
2026-08-18 22:12:23 +0530

    Taskfile

--- go.mod
+++ go.mod
@@ -5,10 +5,14 @@ go 1.26.5
 require (
 	github.com/go-playground/validator/v10 v10.30.3
 	github.com/jackc/pgx/v5 v5.10.0
+	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.15.4
+	golang.org/x/oauth2 v0.36.0
 )
 
 require (
+	cloud.google.com/go/compute/metadata v0.9.0 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/gabriel-vasile/mimetype v1.4.13 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
@@ -19,12 +23,13 @@ require (
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.15 // indirect
 	github.com/mattn/go-isatty v0.0.22 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/crypto v0.53.0 // indirect
-	golang.org/x/net v0.56.0 // indirect
-	golang.org/x/sync v0.21.0 // indirect
-	golang.org/x/sys v0.46.0 // indirect
-	golang.org/x/text v0.38.0 // indirect
+	golang.org/x/crypto v0.54.0 // indirect
+	golang.org/x/net v0.57.0 // indirect
+	golang.org/x/sync v0.22.0 // indirect
+	golang.org/x/sys v0.47.0 // indirect
+	golang.org/x/text v0.40.0 // indirect
 	golang.org/x/time v0.15.0 // indirect
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -2,22 +2,32 @@ package config
 
 import (
 	"fmt"
+	"log/slog"
 	"os"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
 type Config struct {
 	Server   ServerConfig   `validate:"required"`
 	Database DatabaseConfig `validate:"required"`
+	Auth     AuthConfig     `validate:"required"`
 }
 
 type ServerConfig struct {
-	Port string `validate:"required, numeric"`
+	Port string `validate:"required,numeric"`
 }
 
 type DatabaseConfig struct {
 	DatabaseURL string `validate:"required"`
+}
+
+type AuthConfig struct {
+	GoogleOAuth *oauth2.Config
+	JwtSecret   string `validate:"required"`
 }
 
 func validateConfig(cnf *Config) error {
@@ -39,12 +49,28 @@ func GetENV(key string, fallback string) string {
 }
 
 func LoadConfig() (*Config, error) {
+	if err := godotenv.Load(); err != nil {
+		slog.Error("could not load initial env variables")
+		os.Exit(1)
+	}
 	conf := &Config{
 		Server: ServerConfig{
 			Port: GetENV("PORT", "8000"),
 		},
 		Database: DatabaseConfig{
 			DatabaseURL: GetENV("DATABASE_URL", ""),
+		},
+		Auth: AuthConfig{
+			GoogleOAuth: &oauth2.Config{
+				ClientID:     GetENV("GOOGLE_CLIENT_ID", ""),
+				ClientSecret: GetENV("GOOGLE_CLIENT_SECRET", ""),
+				RedirectURL:  GetENV("GOOGLE_REDIRECT_URL", ""),
+				Scopes: []string{
+					"https://www.googleapis.com/auth/userinfo.email",
+					"https://www.googleapis.com/auth/userinfo.profile",
+				},
+				Endpoint: google.Endpoint,
+			},
 		},
 	}
 
